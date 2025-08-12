fd61b2ff0c7d68735d0778177435893c36268071
Aiswarya satheesh <aishuaiswarya2051@gmail.com>
2025-08-12 20:37:35 +0530

    button

--- src/App.js
+++ src/App.js
@@ -1,36 +1,43 @@
-import { useState } from "react";
 import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
 import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
 import BallotIcon from "@mui/icons-material/Ballot";
 import CategoryIcon from "@mui/icons-material/Category";
 import DoNotDisturbIcon from "@mui/icons-material/DoNotDisturb";
+import FormatIndentIncreaseIcon from "@mui/icons-material/FormatIndentIncrease";
 import HomeIcon from "@mui/icons-material/Home";
 import LocalActivityIcon from "@mui/icons-material/LocalActivity";
 import ReorderIcon from "@mui/icons-material/Reorder";
+import { useState } from "react";
 import "./App.css";
 import logo from "./assets/icons/logo.jpg";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isAlways, setIsAlways] = useState(false);
+  const toggleSidebar = () => {
+    setIsAlways(!isAlways);
+    console.log("Is Always", isAlways);
+  };
 
   return (
     <nav
-      className={`navbar ${isOpen ? "open" : "collapsed"}`}
+      className={`navbar ${isAlways || isOpen ? "open" : "collapsed"}`}
       onMouseEnter={() => setIsOpen(true)}
       onMouseLeave={() => setIsOpen(false)}
     >
       <div className="logo-title">
         <img src={logo} alt="Thrillark Logo" className="logo" />
-        {isOpen && <h2 className="title">Thrillark</h2>}
+        {(isAlways || isOpen) && <h2 className="title">Thrillark</h2>}
+        {<FormatIndentIncreaseIcon className="tog" onClick={toggleSidebar} />}
       </div>
       <ul className="nav-links">
         <li className="navbar-item">
           <HomeIcon className="icon" />
-          {isOpen && <span className="text">Dashboard</span>}
+          {(isAlways || isOpen) && <span className="text">Dashboard</span>}
         </li>
         <li className="navbar-item">
           <DoNotDisturbIcon className="icon" />
-          {isOpen && <span className="text">Failed Logs</span>}
+          {(isAlways || isOpen) && <span className="text">Failed Logs</span>}
         </li>
         <li className="navbar-item">
           <CategoryIcon className="icon" />
