61864fb9ea945610b3996c95129c98d548f1f3de
Aiswarya satheesh <aishuaiswarya2051@gmail.com>
2025-08-14 19:16:04 +0530

    temp changes

--- src/components/body/body.jsx
+++ src/components/body/body.jsx
@@ -1,23 +1,27 @@
-import "./body.css";
+import SearchIcon from "@mui/icons-material/Search";
 import { useState } from "react";
+import "./body.css";
 function Body() {
-     const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
 
   const handleSearchChange = (event) => {
     setSearchQuery(event.target.value);
   };
-    return (
-        <div className="body">
-           <div className="search-container">
-      <input
-        type="text"
-        placeholder="Search..."
-        value={searchQuery}
-        onChange={handleSearchChange}
-      />
+  return (
+    <div className="body">
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Search for the experience..."
+          value={searchQuery}
+          className="searchip"
+          onChange={handleSearchChange}
+        />
+        <SearchIcon className="searchicon" />
+        <button />
+        <button />
       </div>
-           
-        </div>
-    );
+    </div>
+  );
 }
 export default Body;
